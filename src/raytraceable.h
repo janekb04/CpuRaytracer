fa62a4023cbf70b6f9d16cd25b0800128a62ec26
janekb04 <janekb04@icloud.com>
2021-04-21 13:43:03 +0200

    refactor intersection code

--- src/raytraceable.h
+++ src/raytraceable.h
@@ -36,13 +36,18 @@ public:
 	[[nodiscard]] std::optional<hit_info> intersect(const ray& r, float t_min, float t_max) const noexcept
 	{ // TODO: this function takes 75% of all processing time. optimize (maybe with SIMD??)
 		const auto transformed_ray = inv_trans * r;
-		auto t = _intersect(transformed_ray);
-		const auto local_pos = transformed_ray.at(t);
-		const auto pos = glm::vec3{ trans.to_mat4() * glm::vec4{ local_pos, 1.0f } };
-		t = signed_length2(pos - r.origin, r.direction);
-		if (t < t_min || t > t_max)
-			return std::nullopt;
-		return hit_info{ t, pos, local_pos, this, transformed_ray };
+		const auto local_pos_opt = _intersect(transformed_ray);
+
+		if(local_pos_opt)
+		{
+			const auto pos = glm::vec3{ trans.to_mat4() * glm::vec4{ *local_pos_opt, 1.0f } };
+			const auto depth = signed_length2(pos - r.origin, r.direction);
+			if (depth < t_min || depth > t_max)
+				return std::nullopt;
+			return hit_info{ depth, pos, *local_pos_opt, this, transformed_ray };
+		}
+		
+		return std::nullopt;
 	}
 	[[nodiscard]] geometry_info hit(const hit_info& hit) const noexcept
 	{
@@ -50,7 +55,7 @@ public:
 	}
 	virtual ~raytraceable() = default;
 protected:
-	[[nodiscard]] virtual float _intersect(const ray& r) const noexcept = 0;
+	[[nodiscard]] virtual std::optional<glm::vec3> _intersect(const ray& r) const noexcept = 0;
 	[[nodiscard]] virtual glm::vec3 _hit(const hit_info& hit) const noexcept = 0;
 };
 
@@ -62,7 +67,7 @@ public:
 	{
 	}
 private:
-	[[nodiscard]] float _intersect(const ray& r) const noexcept override
+	[[nodiscard]] std::optional<glm::vec3> _intersect(const ray& r) const noexcept override
 	{
 		const auto oc = r.origin /* - center */;
 		const auto a = 1.0f;
@@ -71,9 +76,9 @@ private:
 		const auto discriminant = half_b * half_b - a * c;
 
 		if (discriminant < 0) {
-			return -1.0f;
+			return std::nullopt;
 		}
-		return (-half_b - sqrt(discriminant)) / a;
+		return r.at((-half_b - sqrt(discriminant)) / a);
 	}
 	[[nodiscard]] glm::vec3 _hit(const hit_info& hit) const noexcept override
 	{
@@ -81,30 +86,29 @@ private:
 	}
 };
 
-class plane final : public raytraceable
+class plane : public raytraceable
 {
 public:
 	plane(const material& mat, const transform& trans) :
 		raytraceable{mat, trans}
 	{
 	}
-private:
-	[[nodiscard]] float _intersect(const ray& r) const noexcept override
+protected:
+	[[nodiscard]] std::optional<glm::vec3> _intersect(const ray& r) const noexcept override
 	{
 		const auto front_facing = -r.direction.y; // dot(normal, r.direction)
 		if (front_facing < 0) 
 		{
 			const auto dir = /* position */ -r.origin;
 			const auto t = -dir.y / front_facing; // dot(dir, normal) / front_facing
-			return t;
+			return r.at(t);
 		}
 
-		return -1.0f;
+		return std::nullopt;
 	}
 	[[nodiscard]] glm::vec3 _hit(const hit_info& hit) const noexcept override
 	{
 		return { 0, -1, 0}; // normal
 	}
 };
-
 #endif // RAYTRACEABLE_H
